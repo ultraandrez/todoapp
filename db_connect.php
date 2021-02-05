ac06f3c952bfa7acc0769ed8da814a3446698cc4
ultraandrez <48455919+ultraandrez@users.noreply.github.com>
2021-02-05 23:16:11 +0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new mysqli('localhost','mysql','mysql','todo_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('remotemysql.com','Bi25Netqw8','mlFm8N0546','Bi25Netqw8')or die("Could not connect to mysql".mysqli_error($con));
 ?>
