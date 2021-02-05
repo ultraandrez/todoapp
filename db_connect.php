56010cdae342e961c16271e9970ac8b5b45a530d
ultraandrez <48455919+ultraandrez@users.noreply.github.com>
2021-02-05 23:31:12 +0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,10 @@
 <?php
-$servername = "remotemysql.com";
-$username = "Bi25Netqw8";
-$password = "mlFm8N0546";
-$bdname = "Bi25Netqw8";
-$conn = new PDO("mysql:host=$servername;dbname=$bdname", $username, $password);
+$servername = 'remotemysql.com';
+$username = 'Bi25Netqw8';
+$password = 'mlFm8N0546';
+$bdname = 'Bi25Netqw8';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$servername;dbname=$bdname;charset=$charset";
+$conn = new PDO($dsn, $username, $password);
 ?>
