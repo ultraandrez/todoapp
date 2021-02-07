95417e24a0c18f19edd7471d9f826413edd7982d
ultraandrez <48455919+ultraandrez@users.noreply.github.com>
2021-02-08 02:58:43 +0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,5 @@
 <?php
 $conn = new mysqli('localhost','mysql','mysql','todo_db')or die("Could not connect to mysql".mysqli_error($con));
+$charset = "utf8";
+mysqli_set_charset($conn, $charset);
 ?>
